d80f5a93d430d39fb3120448a3b52f0d0e56a13b
ted9219 <ted9219@gmail.com>
2021-03-02 04:46:37 +0000

    add cohortDiagnostics function

--- R/CreateCohorts.R
+++ R/CreateCohorts.R
@@ -1,6 +1,6 @@
 # Copyright 2020 Observational Health Data Sciences and Informatics
 #
-# This file is part of RanitidineCancerRisk
+# This file is part of Argos
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -24,7 +24,7 @@
   
   # Create study cohort table structure:
   sql <- SqlRender::loadRenderTranslateSql(sqlFilename = "CreateCohortTable.sql",
-                                           packageName = "RanitidineCancerRisk",
+                                           packageName = "Argos",
                                            dbms = attr(connection, "dbms"),
                                            oracleTempSchema = oracleTempSchema,
                                            cohort_database_schema = cohortDatabaseSchema,
@@ -32,19 +32,38 @@
   DatabaseConnector::executeSql(connection, sql, progressBar = FALSE, reportOverallTime = FALSE)
   
   
+  # Insert rule names in cohort_inclusion table:
+  pathToCsv <- system.file("cohorts", "InclusionRules.csv", package = "Argos")
+  inclusionRules <- readr::read_csv(pathToCsv, col_types = readr::cols()) 
+  inclusionRules <- data.frame(cohort_definition_id = inclusionRules$cohortId,
+                               rule_sequence = inclusionRules$ruleSequence,
+                               name = inclusionRules$ruleName)
+  DatabaseConnector::insertTable(connection = connection,
+                                 tableName = "#cohort_inclusion",
+                                 data = inclusionRules,
+                                 dropTableIfExists = TRUE,
+                                 createTable = TRUE,
+                                 tempTable = TRUE,
+                                 oracleTempSchema = oracleTempSchema)
+  
   
   # Instantiate cohorts:
-  pathToCsv <- system.file("settings", "CohortsToCreate.csv", package = "RanitidineCancerRisk")
-  cohortsToCreate <- read.csv(pathToCsv)
+  pathToCsv <- system.file("settings", "CohortsToCreate.csv", package = "Argos")
+  cohortsToCreate <- readr::read_csv(pathToCsv, col_types = readr::cols())
   for (i in 1:nrow(cohortsToCreate)) {
     writeLines(paste("Creating cohort:", cohortsToCreate$name[i]))
     sql <- SqlRender::loadRenderTranslateSql(sqlFilename = paste0(cohortsToCreate$name[i], ".sql"),
-                                             packageName = "RanitidineCancerRisk",
+                                             packageName = "Argos",
                                              dbms = attr(connection, "dbms"),
                                              oracleTempSchema = oracleTempSchema,
                                              cdm_database_schema = cdmDatabaseSchema,
                                              vocabulary_database_schema = vocabularyDatabaseSchema,
-                                                
+                                             
+                                             results_database_schema.cohort_inclusion = "#cohort_inclusion",  
+                                             results_database_schema.cohort_inclusion_result = "#cohort_inc_result",  
+                                             results_database_schema.cohort_inclusion_stats = "#cohort_inc_stats",  
+                                             results_database_schema.cohort_summary_stats = "#cohort_summary_stats",  
+                                             
                                              target_database_schema = cohortDatabaseSchema,
                                              target_cohort_table = cohortTable,
                                              target_cohort_id = cohortsToCreate$cohortId[i])
@@ -53,16 +72,40 @@
   
   # Fetch cohort counts:
   sql <- "SELECT cohort_definition_id, COUNT(*) AS count FROM @cohort_database_schema.@cohort_table GROUP BY cohort_definition_id"
-  sql <- SqlRender::renderSql(sql,
-                              cohort_database_schema = cohortDatabaseSchema,
-                              cohort_table = cohortTable)$sql
-  sql <- SqlRender::translateSql(sql, targetDialect = attr(connection, "dbms"))$sql
+  sql <- SqlRender::render(sql,
+                           cohort_database_schema = cohortDatabaseSchema,
+                           cohort_table = cohortTable)
+  sql <- SqlRender::translate(sql, targetDialect = attr(connection, "dbms"))
   counts <- DatabaseConnector::querySql(connection, sql)
   names(counts) <- SqlRender::snakeCaseToCamelCase(names(counts))
   counts <- merge(counts, data.frame(cohortDefinitionId = cohortsToCreate$cohortId,
                                      cohortName  = cohortsToCreate$name))
-  write.csv(counts, file.path(outputFolder, "CohortCounts.csv"))
+  readr::write_csv(x = counts, file = file.path(outputFolder, "CohortCounts.csv"))
   
+  
+  # Fetch inclusion rule stats and drop tables:
+  fetchStats <- function(tableName) {
+    sql <- "SELECT * FROM #@table_name"
+    sql <- SqlRender::render(sql, table_name = tableName)
+    sql <- SqlRender::translate(sql = sql, 
+                                targetDialect = attr(connection, "dbms"),
+                                oracleTempSchema = oracleTempSchema)
+    stats <- DatabaseConnector::querySql(connection, sql)
+    names(stats) <- SqlRender::snakeCaseToCamelCase(names(stats))
+    fileName <- file.path(outputFolder, paste0(SqlRender::snakeCaseToCamelCase(tableName), ".csv"))
+    readr::write_csv(x = stats, file = fileName)
+    
+    sql <- "TRUNCATE TABLE #@table_name; DROP TABLE #@table_name;"
+    sql <- SqlRender::render(sql, table_name = tableName)
+    sql <- SqlRender::translate(sql = sql, 
+                                targetDialect = attr(connection, "dbms"),
+                                oracleTempSchema = oracleTempSchema)
+    DatabaseConnector::executeSql(connection, sql)
+  }
+  fetchStats("cohort_inclusion")
+  fetchStats("cohort_inc_result")
+  fetchStats("cohort_inc_stats")
+  fetchStats("cohort_summary_stats")
   
 }
 
--- extras/codeToRun.r
+++ extras/codeToRun.r
@@ -12,6 +12,7 @@ cohortDatabaseSchema <- "ONCOACHILLES.dbo"
 cohortTable <- "argos_cohort_0207"
 outputFolder <- "~/output/argos"
 options(fftempdir = "~/FFtemp")
+options(andromedaTempFolder = "~/AndromedaTemp")
 
 if (!file.exists(outputFolder))
     dir.create(outputFolder, recursive = TRUE)
@@ -67,6 +68,61 @@ ParallelLogger::addDefaultFileLogger(file.path(outputFolder, "log.txt"))
 
 #Connection
 connection<-DatabaseConnector::connect(connectionDetails)
+
+#####CohortDiagnostics#########
+
+# For Oracle: define a schema that can be used to emulate temp tables:
+oracleTempSchema <- NULL
+
+# Details specific to the database:
+databaseId <- "CCAE"
+databaseName <- "IBM MarketScan Commercial Claims and Encounters Database"
+databaseDescription <- "IBM MarketScan® Commercial Claims and Encounters Database (CCAE) represent data from individuals enrolled in United States employer-sponsored insurance health plans. The data includes adjudicated health insurance claims (e.g. inpatient, outpatient, and outpatient pharmacy) as well as enrollment data from large employers and health plans who provide private healthcare coverage to employees, their spouses, and dependents. Additionally, it captures laboratory tests for a subset of the covered lives. This administrative claims database includes a variety of fee-for-service, preferred provider organizations, and capitated health plans." 
+
+# Use this to run the cohorttDiagnostics. The results will be stored in the diagnosticsExport subfolder of the outputFolder. This can be shared between sites.
+Argos::runCohortDiagnostics(connectionDetails = connectionDetails,
+                                     cdmDatabaseSchema = cdmDatabaseSchema,
+                                     cohortDatabaseSchema = cohortDatabaseSchema,
+                                     cohortTable = cohortTable,
+                                     oracleTempSchema = oracleTempSchema,
+                                     outputFolder = outputFolder,
+                                     databaseId = databaseId,
+                                     databaseName = databaseName,
+                                     databaseDescription = databaseDescription,
+                                     createCohorts = TRUE,
+                                     runInclusionStatistics = TRUE,
+                                     runIncludedSourceConcepts = TRUE,
+                                     runOrphanConcepts = TRUE,
+                                     runTimeDistributions = TRUE,
+                                     runBreakdownIndexEvents = TRUE,
+                                     runIncidenceRates = TRUE,
+                                     runCohortOverlap = TRUE,
+                                     runCohortCharacterization = TRUE,
+                                     runTemporalCohortCharacterization = TRUE,
+                                     minCellCount = 5)
+
+# To view the results:
+# Optional: if there are results zip files from multiple sites in a folder, this merges them, which will speed up starting the viewer:
+CohortDiagnostics::preMergeDiagnosticsFiles(file.path(outputFolder, "diagnosticsExport"))
+
+# Use this to view the results. Multiple zip files can be in the same folder. If the files were pre-merged, this is automatically detected: 
+CohortDiagnostics::launchDiagnosticsExplorer(file.path(outputFolder, "diagnosticsExport"))
+
+
+# To explore a specific cohort in the local database, viewing patient profiles:
+# CohortDiagnostics::launchCohortExplorer(connectionDetails = connectionDetails,
+#                                         cdmDatabaseSchema = cdmDatabaseSchema,
+#                                         cohortDatabaseSchema = cohortDatabaseSchema,
+#                                         cohortTable = cohortTable,
+#                                         cohortId = 123)
+# Where 123 is the ID of the cohort you wish to inspect.
+
+###############################
+
+
+
+
+
 
  
  ####Create cohort####
